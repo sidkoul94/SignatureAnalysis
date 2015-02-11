46f5697b90936cafae0f568e7960792e7032bf1e
Siddanth <sidkoul94@gmail.com>
2015-02-11 19:31:01 +0530

    Frame Initializer Class

---
+++ src/Display/CreateWindow.java
@@ -0,0 +1,25 @@
+package Display;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+
+public class CreateWindow {
+
+	private void CreateWindow() {
+	}
+
+	public static void ofImage(BufferedImage Image, String Title) {
+
+		JFrame newFrame = new JFrame(Title);
+		Jlabel picture = new Jlabel(Image);
+		newFrame.add(picture, BorderLayout.CENTER);
+		newFrame.pack();
+		newFrame.setLocationRelativeTo(null);
+		newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		newFrame.setVisible(true);
+
+	}
+
+}
