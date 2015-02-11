3ad3d9a74d6bdcc9115b3bdeab713bfb5b3185f5
Siddanth <sidkoul94@gmail.com>
2015-02-11 19:31:42 +0530

    Added PrefferedSize Constraint

--- src/Display/Jlabel.java
+++ src/Display/Jlabel.java
@@ -1,16 +1,20 @@
 package Display;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class Jlabel extends JLabel {
 
 	BufferedImage InputImage;
-
+	int S2= 50;
 	public Jlabel(BufferedImage InputImage) {
 		this.InputImage  = InputImage;
+		this.setBorder(BorderFactory.createEmptyBorder(S2, S2, S2, S2));
+		this.setPreferredSize(new Dimension(InputImage.getWidth(),InputImage.getHeight()));
 	}
 
 	public void paint(Graphics g) {
