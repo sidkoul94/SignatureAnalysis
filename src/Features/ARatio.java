15f494e1fe3a5181270abade2c07c82daa9c0bf1
Siddanth <sidkoul94@gmail.com>
2015-02-15 21:39:20 +0530

    Code optimization

--- src/Features/ARatio.java
+++ src/Features/ARatio.java
@@ -1,5 +1,6 @@
 package Features;
-import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 public class ARatio {
 	
@@ -8,8 +9,8 @@ public class ARatio {
 	public int topy;
 	
 	//Coordinates of bottom right corner
-	int bottomx;
-	int bottomy;
+	public int bottomx;
+	public int bottomy;
 
 	//get the width of the InputImage
 	public int getWidth() {
@@ -36,5 +37,83 @@ public class ARatio {
 	public float getAspectRatio() {
 		return (float) ((float)(bottomx - topx) / (bottomy - topy));
 	}
+	
+	// Calculating the Aspect Ratio
+		public ARatio getARatio(BufferedImage InputImage) {
+
+			int height, width;
+			height = InputImage.getHeight();
+			width = InputImage.getWidth();
+
+			// Retrieve Lefty
+			int lefty = 0;
+			boolean flag = false;
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					if ((InputImage.getRGB(i, j)) == Color.BLACK.getRGB()) {
+						lefty = i;
+						flag = true;
+						break;
+					}
+					if (flag)
+						break;
+				}
+			}
+
+			// Retrieve Righty
+			int righty = 0;
+			flag = false;
+			for (int i = width - 1; i >= 0; i--) {
+				for (int j = 0; j < height; j++) {
+					if ((InputImage.getRGB(i, j)) == Color.BLACK.getRGB()) {
+						righty = i;
+						flag = true;
+						break;
+					}
+
+					if (flag)
+						break;
+				}
+			}
+
+			// Retrieve MaxXx
+			int topx = 0;
+			flag = false;
+			for (int j = 0; j < height; j++) {
+				for (int i = 0; i < width; i++) {
+					if ((InputImage.getRGB(i, j)) == Color.BLACK.getRGB()) {
+						topx = j;
+						flag = true;
+						break;
+					}
+
+					if (flag)
+						break;
+				}
+			}
+
+			// Retrieve MinX
+			int bottomx = 0;
+			flag = false;
+			for (int j = height - 1; j >= 0; j--) {
+				for (int i = 0; i < width; i++) {
+					if ((InputImage.getRGB(i, j)) == Color.BLACK.getRGB()) {
+						bottomx = j;
+						flag = true;
+						break;
+					}
+
+					if (flag)
+						break;
+				}
+			}
+
+			width = righty - lefty;
+			height = bottomx - topx;
+			ARatio ar = new ARatio();
+			ar.setTopLeft(lefty, topx);
+			ar.setBottomRight(righty, bottomx);
+			return ar;
+		}
 
 }
